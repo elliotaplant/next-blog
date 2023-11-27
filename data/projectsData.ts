10c9d21e7819fccdf6dc7d4dd9e36c54a6711b47
Elliot Plant <elliotaplant@gmail.com>
2023-11-26 20:39:34 -0800

    More posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,6 +1,15 @@
 const projectsData = [
   {
     title: 'Turn Ethereum Addresses into Etherscan Links in iTerm2',
+    description: `When Peleton decided to make a stationary bike with a screen, why didn't they add games? Wouldn't
+  you be more motivated to cycle on Rainbow Road than a virtual spin class? In a very roundabout
+  effort to gamify exercise, I decided to make a stationary bike that acted as a Mario Kart 64
+  controller.`,
+    imgSrc: '/static/images/mario-bike-buttons.jpg',
+    href: 'https://elliotplant.co/blog/mario-kart-bike',
+  },
+  {
+    title: 'Turn Ethereum Addresses into Etherscan Links in iTerm2',
     description: `Tired of copying and pasting eth addresses into etherscan's search from your terminal? Try
   turning each address and transaction hash into a link straight to etherscan.`,
     imgSrc: '/static/images/etherscan-iterm.png',
@@ -11,6 +20,14 @@ const projectsData = [
     description: `Abra is an incredibly small and simple sort-of-react-like JS "framework". Definitely not production ready.`,
     imgSrc: '/static/images/abra.png',
     href: 'https://medium.com/@elliotaplant/abra-js-1c1e51f186c2',
+  },
+  {
+    title: 'Solving Puzzlemaster Presents with JavaScript: April 15th',
+    description: `I love solving puzzles, I love JavaScript, and I listen to Will Shortz' Sunday Puzzle almost every week. 
+    Oftentimes, I find myself thinking of ways I could solve Will's puzzles with code. 
+    This post, and the ones that follow, are a way for me to combine my passions and hopefully teach a bit of JavaScript and algorithms.`,
+    imgSrc: '/static/images/puzzlemaster.jpeg',
+    href: 'https://medium.com/@elliotaplant/solving-puzzlemaster-presents-with-javascript-april-15th-c0923b664a16',
   },
 ];
 
